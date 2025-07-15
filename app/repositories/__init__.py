23f59676773fd91bb88c954f79d9ad2ac7097d80
Aleksandr Timokhin <adtimokhin@gmail.com>
2025-07-15 10:13:59 +0200

    added a sample file in the repository module

---
+++ app/repositories/__init__.py
@@ -0,0 +1,3 @@
+"""
+This package contains the repositories for the application.
+"""
---
+++ app/repositories/user.py
@@ -0,0 +1,6 @@
+"""
+This module contains the functions to interact with the user table.
+All of the interaction with the user table should be done through this module.
+"""
+
+# TODO: Implement the functions to interact with the user table.
