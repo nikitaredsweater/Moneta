9dd6cdc46669e422e3b884f31f77db1531357653
Aleksandr Timokhin <adtimokhin@gmail.com>
2025-07-28 16:01:40 +0200

    added structure of files that will allow to control user permissions based on their role

--- app/enums/__init__.py
+++ app/enums/__init__.py
@@ -1,7 +1,7 @@
 """Enums entrypoint"""
 
 from app.enums.order_types import OrderType
+from app.enums.permissions import PermissionEntity, PermissionVerb
+from app.enums.roles import UserRole
 
-__all__ = [
-    'OrderType',
-]
+__all__ = ['OrderType', 'PermissionVerb', 'PermissionEntity', 'UserRole']
---
+++ app/security/permissions.py
@@ -0,0 +1,73 @@
+"""
+security module related to the work with the roles and permissions
+"""
+
+from dataclasses import dataclass
+from typing import Iterable
+
+from app.enums import PermissionEntity as Entity
+from app.enums import PermissionVerb as Verb
+from app.enums import UserRole
+
+
+@dataclass(frozen=True)
+class Permission:
+    verb: Verb
+    entity: Entity
+
+    def __str__(self) -> str:
+        return f'{self.verb}.{self.entity}'
+
+
+# Note:
+#
+# Current implementation limits the ability of 'dynamic' user permissions.
+# Once you are assigned to a role, only way to get expanded permissions on the
+# Platform for a specific user is to allow the whole role more permissions
+# This may be the inteded behavior, but I am not sure.
+
+ROLE_PERMISSIONS = {
+    UserRole.ADMIN: {
+        # Global data access
+        (Verb.VIEW, Entity.ALL_DATA),
+        # User management
+        (Verb.VIEW, Entity.ALL_USERS),
+        (Verb.CREATE, Entity.USER),
+        (Verb.UPDATE, Entity.USER),
+        (Verb.DELETE, Entity.ALL_USERS),
+        # Role management
+        (Verb.VIEW, Entity.ALL_ROLES),
+        (Verb.ASSIGN, Entity.ROLE),
+        # Company
+        (Verb.VIEW, Entity.COMPANY),
+        (Verb.CREATE, Entity.COMPANY),
+        (Verb.UPDATE, Entity.COMPANY),
+        (Verb.DELETE, Entity.COMPANY),
+        # Company address
+        (Verb.VIEW, Entity.COMPANY_ADDRESS),
+        (Verb.CREATE, Entity.COMPANY_ADDRESS),
+        (Verb.UPDATE, Entity.COMPANY_ADDRESS),
+        (Verb.DELETE, Entity.COMPANY_ADDRESS),
+    },
+    UserRole.BUYER: {
+        (Verb.VIEW, Entity.COMPANY),
+        (Verb.VIEW, Entity.COMPANY_ADDRESS),
+    },
+    UserRole.SELLER: {
+        (Verb.VIEW, Entity.COMPANY),
+        (Verb.VIEW, Entity.COMPANY_ADDRESS),
+    },
+    UserRole.ISSUER: {
+        (Verb.VIEW, Entity.COMPANY),
+        (Verb.UPDATE, Entity.COMPANY),
+        (Verb.VIEW, Entity.COMPANY_ADDRESS),
+        (Verb.CREATE, Entity.COMPANY_ADDRESS),
+        (Verb.UPDATE, Entity.COMPANY_ADDRESS),
+        (Verb.DELETE, Entity.COMPANY_ADDRESS),
+    },
+}
+
+
+def has_permission(role: UserRole, required: Iterable[Permission]) -> bool:
+    allowed = ROLE_PERMISSIONS.get(role, set())
+    return any(p in allowed for p in required)
