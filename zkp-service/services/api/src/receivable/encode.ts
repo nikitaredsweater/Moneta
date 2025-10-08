820a7783c389fb2fb5957ac3add9196afd6c9c1a
Aleksandr Timokhin <adtimokhin@gmail.com>
2025-10-08 12:23:42 -0500

    added an endpoint that checks fields for a receivable create request and responses with an error or OK

--- zkp-service/services/api/src/receivable/encode.ts
+++ zkp-service/services/api/src/receivable/encode.ts
@@ -1,4 +1,5 @@
-import { FieldSpec, FieldType } from "./types";
+import type { FieldSpec} from "./types";
+import { FieldType } from "./types";
 
 const UUID_V4_RE =
   /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
